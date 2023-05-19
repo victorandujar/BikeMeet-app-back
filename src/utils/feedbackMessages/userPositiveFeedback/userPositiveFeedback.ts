00ba60f692bb8e25eaebf294efdf7cebeedbabab
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-05-19 15:36:16 +0200

    Resolve code smell

--- src/utils/feedbackMessages/userPositiveFeedback/userPositiveFeedback.ts
+++ src/utils/feedbackMessages/userPositiveFeedback/userPositiveFeedback.ts
@@ -1,5 +1,7 @@
-import { type UserFeedbackStructure } from "../types/types";
-import { type UsersPositiveStatusCodesStructure } from "../types/types";
+import {
+  type UserFeedbackStructure,
+  type UsersPositiveStatusCodesStructure,
+} from "../types/types";
 
 export const userPositiveFeedback: UserFeedbackStructure = {
   userCreatedMessage: "The user has been created!",
