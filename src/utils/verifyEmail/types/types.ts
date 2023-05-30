6f194c2c37714985af5dd22a34db0a58141a501f
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-05-30 12:41:41 +0200

    Create email transporter at createMailTransporter

---
+++ src/utils/verifyEmail/types/types.ts
@@ -0,0 +1,11 @@
+export interface UserAdminCredentials {
+  user: string;
+  password: string;
+}
+
+export interface UserMailOptionsStructure {
+  from: string;
+  to: string;
+  subject: string;
+  html: string;
+}
