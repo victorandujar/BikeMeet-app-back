7f6733e489bcb1a42fbc3d8dc1ed249dd6643df8
victorandujar <victorandujarmurcia@gmail.com>
2023-06-04 11:23:28 +0200

    Add endpoint for checking if verified user at router path

--- src/server/routers/userRouter/userRouter.ts
+++ src/server/routers/userRouter/userRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import endpoints from "../../../utils/endpoints/endpoints.js";
 import {
+  getUserCheckVerified,
   loginUser,
   registerUser,
   verifyEmail,
@@ -11,5 +12,6 @@ const usersRouter = Router();
 usersRouter.post(endpoints.register, registerUser);
 usersRouter.post(endpoints.login, loginUser);
 usersRouter.post(endpoints.verifyEmail, verifyEmail);
+usersRouter.post(endpoints.getUser, getUserCheckVerified);
 
 export default usersRouter;
