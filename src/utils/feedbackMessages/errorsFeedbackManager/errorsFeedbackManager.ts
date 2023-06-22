9c108a8efb7e907444af4d75775c43aeebf9f1a8
victorandujar <victorandujarmurcia@gmail.com>
2023-06-22 13:28:41 +0200

    Add feedback messages for api response while requesting user data

--- src/utils/feedbackMessages/errorsFeedbackManager/errorsFeedbackManager.ts
+++ src/utils/feedbackMessages/errorsFeedbackManager/errorsFeedbackManager.ts
@@ -6,8 +6,8 @@ import {
 
 export const userErrorsManagerMessages: UserErrorsManagerMessagesStructure = {
   server: "Error on starting the server!",
-  notFoundDev: "Path not found.",
-  notFoundUser: "Endpoint not found.",
+  notFoundDev: "Path not found",
+  notFoundUser: "Endpoint not found",
   publicMessageDefault: "Something went wrong!. Try again.",
   registerPublicMessage: "The user couldn't be created. Try again!",
   wrongCredentialsMessage: "Wrong credentials",
