83a2c15618f6467a10408de1fe8b6b6d9c66e012
victorandujar <victorandujarmurcia@gmail.com>
2023-06-22 13:27:46 +0200

    Create route for getUser controller at userRouter

--- src/utils/endpoints/types/types.ts
+++ src/utils/endpoints/types/types.ts
@@ -3,10 +3,11 @@ export interface UserEndpointsStructure {
   register: string;
   login: string;
   verifyEmail: string;
-  getUser: string;
+  userToVerify: string;
   recoveryPassword: string;
   resetPasswordEmail: string;
   restorePassword: string;
+  getUser: string;
 }
 
 export interface RidesEndpointsStructure {
