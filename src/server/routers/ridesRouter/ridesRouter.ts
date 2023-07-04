9feda816a7085e0d1c9c80990a9e1d8e136b690d
Róman Demchuk <roman.demchuk@gmail.com>
2023-07-04 16:52:03 +0200

    Add createRide to ridesRouter

--- src/server/routers/ridesRouter/ridesRouter.ts
+++ src/server/routers/ridesRouter/ridesRouter.ts
@@ -3,11 +3,13 @@ import { ridesEndpoints } from "../../../utils/endpoints/endpoints.js";
 import {
   getAllRides,
   getRideById,
+  createRide,
 } from "../../controllers/ridesControllers/ridesControllers.js";
 
 const ridesRouter = Router();
 
 ridesRouter.get(ridesEndpoints.getAllRides, getAllRides);
 ridesRouter.get(ridesEndpoints.detail, getRideById);
+ridesRouter.post(ridesEndpoints.create, createRide);
 
 export default ridesRouter;
